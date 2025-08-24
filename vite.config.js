995d7941a0b837ecb60e2541b357143c89b670e1
Amar mauraya <amarmauraya@Amars-MacBook-Air.local>
2025-08-24 21:40:23 +0530

    save

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "dist", // 👈 changed from build → dist
+    outDir: "dist", // 👈 use dist so Vercel auto-detects
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
